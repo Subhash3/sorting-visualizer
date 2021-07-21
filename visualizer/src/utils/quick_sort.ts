b4366dad42fe76df2050dea8227836c65cc8595b
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-07-21 11:37:25 +0530

    Quick sorted! With proper animations

--- visualizer/src/utils/quick_sort.ts
+++ visualizer/src/utils/quick_sort.ts
@@ -1,30 +1,54 @@
-import { animate, inPlaceSwap } from './sort';
+import { animate, highlightBars, inPlaceSwap } from './sort';
 import { IFC_BarsInfo } from "../components/BarsContainer/BarsContainer"
 
 export const quickSort = async (barsInfo: IFC_BarsInfo, setBarsInfo: React.Dispatch<React.SetStateAction<IFC_BarsInfo>>, speed: number) => {
+    let delay = 1 / speed
 
     const quickSortUtil = async (arr: number[], left: number, right: number) => {
         if (left >= right) {
             return
         }
 
+        let highlighted = barsInfo.colors
+        highlightBars(highlighted, [left], "yellow")
+        highlightBars(highlighted, [right], "blue")
+
         let pivotIndex = await pivotPartition(arr, left, right)
         await quickSortUtil(arr, left, pivotIndex - 1)
         await quickSortUtil(arr, pivotIndex + 1, right)
     }
     const pivotPartition = async (arr: number[], left: number, right: number) => {
+        let highlighted = barsInfo.colors
+
         let pivotIndex = right
         let pivot = arr[pivotIndex]
 
+        let pivotHighlighted = highlightBars(highlighted, [pivotIndex], "purple")
+
         let i = left
         let j
+
         for (j = left; j < right; j++) {
+            let pointersHighlighted = pivotHighlighted
+            pointersHighlighted = highlightBars(pointersHighlighted, [i], "orange")
+            pointersHighlighted = highlightBars(pointersHighlighted, [j], "magenta")
+            await animate(arr, pointersHighlighted, setBarsInfo, delay)
+
             if (arr[j] < pivot) {
+                pointersHighlighted = highlightBars(pointersHighlighted, [i], "red")
+                await animate(arr, pointersHighlighted, setBarsInfo, delay)
+
                 inPlaceSwap(i, j, arr)
+
+                pointersHighlighted = highlightBars(pointersHighlighted, [i], "lightgreen")
+                await animate(arr, pointersHighlighted, setBarsInfo, delay)
+
                 i += 1
             }
         }
         inPlaceSwap(i, j, arr)
+        pivotHighlighted = highlightBars(highlighted, [i], "lightgreen")
+        await animate(arr, pivotHighlighted, setBarsInfo, delay)
         pivotIndex = i
 
         return pivotIndex
